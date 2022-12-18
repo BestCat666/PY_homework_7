578302a44c8ce22cf0d48fba967853fbd1751738
Best Cat <kotova-irina11@rambler.ru>
2022-12-18 15:29:26 +0300

    меню для выбора команд

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import getting
 import reading
 import import_inform
 import export_inform
-operation = int(input('Введите номер операции: 1 - добавление записи, 2 - вывод записи на экран, 3 - импорт, 4 - экспорт): '))
-if operation == 1:
-    getting.get()
-if operation == 2:
-    reading.read()
-if operation == 3:
-    import_inform.import_()
-if operation == 4:
-    export_inform.export_()
+while True:
+    operation = int(input('Введите номер операции: 1 - добавление записи, 2 - вывод записи на экран, 3 - импорт, 4 - экспорт, 5 - выход из программы): '))
+    if operation == 1:
+        getting.get()
+    if operation == 2:
+        reading.read()
+    if operation == 3:
+        import_inform.import_()
+    if operation == 4:
+        export_inform.export_()
+    if operation == 5:
+        break
